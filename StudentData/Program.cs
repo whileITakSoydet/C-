c67802460d8dc25ea50a3a4d52c5137ca017b8c8
whileITakSoydet <118098835+whileITakSoydet@users.noreply.github.com>
2023-05-29 11:40:29 +0300

    Add files via upload

---
+++ StudentData/Program.cs
@@ -0,0 +1,43 @@
+﻿using lessonClassWork;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Student stud = new Student();
+
+        Console.WriteLine("Введите номер группы: ");
+        stud.Group = Console.ReadLine();
+        Console.WriteLine("Введите имя студента: ");
+        stud.Name = Console.ReadLine();
+        Console.WriteLine("Введите возраст студента: ");
+        int.TryParse(Console.ReadLine(), out int age);
+        stud.Age = age;
+        Console.WriteLine("Введите оценки(у) по первому предмету если введете 0 или не число ввод прекратиться:");
+        setMark(stud, 0);
+        Console.WriteLine("Введите оценки(у) по второму предмету если введете 0 или не число ввод прекратиться:");
+        setMark(stud, 1);
+        Console.WriteLine("Введите оценки(у) по третьему предмету если введете 0 или не число ввод прекратиться:");
+        setMark(stud, 2);
+        Console.WriteLine("\n\tДанные по студенту:");
+        stud.ShowStudent();
+        Console.ReadKey();
+    }
+    private static void setMark(Student stud, int subject)
+    {
+        while (true)
+        {
+            Console.Write("Ввод: ");
+            int.TryParse(Console.ReadLine(), out int mark);
+            if (mark == 0)
+            {
+                break;
+            }
+            else
+            {
+                stud.SetScore(subject, mark);
+            }
+        };
+    }
+
+}
---
+++ StudentData/Student.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lessonClassWork
+{
+    internal class Student
+    {
+        private string _name;
+        private string _group;
+        private int _age;
+        private int[][] _score;
+        public string Name
+        {
+            get => _name;
+            set{ _name = value.Length < 2 ? "Нет имени" : value; }
+        }
+        public Student()
+        {
+            _score = new int[3][];
+        }
+        public string Group
+        {
+            get => _group;
+            set { _group = value; }
+        }
+        public int Age
+        {
+            get => _age;
+            set
+            {
+                _age = value > 0 && value <= 120 ? value : 0;
+            }
+        }
+        public void SetScore(int subject, params int[] arr)
+        {
+            if (_score[subject] == null)
+            {
+                _score[subject] = new int[arr.Length];
+                _score[subject] = arr;
+            }
+            else
+            {
+                _score[subject] = _score[subject].Concat(arr).ToArray();
+            }
+        }
+        public int[] GetScore(int subject)
+        {
+            return _score[subject];
+        }
+        public double AverageScore(int subject)
+        {
+            if (_score[subject].Length == 1)
+            {
+                return _score[subject][0];
+            }
+            double sum = _score[subject].Sum();
+            return sum / _score[subject].Length;            
+        }
+        public bool ShowScore(int subject)
+        {
+            if (_score[subject] == null)
+            {
+                Console.WriteLine("По этому предмету оценки отсутствуют");
+                return false;
+            }
+            foreach (int i in _score[subject]) 
+            {
+                Console.Write($"{i}\t");
+            }
+            return true;
+        }
+        public void ShowStudent()
+        {
+            Console.WriteLine($"Группа: {_group}, студент: {_name}, возрастом: {_age}");
+            for(int i = 0; i < _score.Length; ++i)
+            {
+                Console.WriteLine($"Предмет номер: {i + 1}");
+                if(ShowScore(i))
+                {
+                    Console.WriteLine($"\nСредний бал: {AverageScore(i)}");
+                }
+            }
+        }
+    }
+}
