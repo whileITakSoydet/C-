b100a67a1bba5b049cf5fa9779e03c724db0ecad
whileITakSoydet <118098835+whileITakSoydet@users.noreply.github.com>
2023-06-05 16:51:35 +0300

    Add files via upload

---
+++ IndexArrays/IndexArrays.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndexingArrays
+{
+    internal class IndexArrays<T>
+    {
+        private int _min;
+        private int _max;
+        private T[] _array;
+        public IndexArrays(int min, int max) 
+        {
+            _min = min;
+            _max = max;
+            _array = new T[_max - _min];
+        }
+        public int GetMin()
+        {
+            return _min;
+        }
+        public int GetMax() 
+        {
+            return _max;   
+        }
+        public int Length()
+        {
+            return _max;
+        }        
+        public T this[int index]
+        {
+            get
+            {
+                if(index >= _min && index < _max)
+                {
+                    return _array[index - _min];
+                }
+                else
+                {
+                    throw new IndexOutOfRangeException();
+                }
+            }
+            set 
+            {
+                if (index >= _min && index < _max)
+                {
+                    _array[index - _min] = value;
+                }
+                else
+                {
+                    throw new IndexOutOfRangeException();
+                }
+            }
+        }
+    }
+}
---
+++ IndexArrays/Program.cs
@@ -0,0 +1,19 @@
+﻿using IndexingArrays;
+
+internal class Program
+{
+    private static void Main()
+    {
+        Console.WriteLine("Выберите нижний порог итератора:");
+        int min = int.Parse(Console.ReadLine());
+        Console.WriteLine("Выберите верхний порог итератора:");
+        int max = int.Parse(Console.ReadLine());
+        Random random = new Random();
+        IndexArrays<int> arr = new IndexArrays<int>(min, max);
+        for (int i = arr.GetMin(); i < arr.Length(); ++i)
+        {
+            arr[i] = random.Next(100);
+            Console.WriteLine(arr[i]);
+        }
+    }
+}
