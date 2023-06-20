8501ea23521145310190efecab932c1fe7c2d843
whileITakSoydet <118098835+whileITakSoydet@users.noreply.github.com>
2023-06-20 21:29:45 +0300

    Add files via upload

---
+++ CoutryDictionary/Glossary.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountryDictionary
+{
+    internal class Glossary
+    {
+        private Dictionary<string, string> _dictionary;
+        public Glossary()
+        {
+            _dictionary = new Dictionary<string, string>();
+            _dictionary.Add("Россия", "Russia");
+            _dictionary.Add("США", "USA");
+            _dictionary.Add("Китай", "China");
+            _dictionary.Add("Германия", "Germany");
+            _dictionary.Add("Франция", "French");
+        }
+        public void AddWords(string first, string second)
+        {
+            _dictionary.Add(first, second);
+        }
+        public void DellRussianWords(string key)
+        {
+            if (_dictionary.ContainsKey(key))
+            {
+                _dictionary.Remove(key);
+            }
+            else
+            {
+                throw new Exception("Слово отсутствует в словаре");
+            }
+        }
+        public void DellEnglishWords(string key)
+        {
+            if (_dictionary.ContainsValue(key))
+            {
+                foreach (var item in _dictionary)
+                {
+                    if (item.Value == key)
+                    {
+                        key = item.Key;
+                        break;
+                    }
+                }
+                _dictionary.Remove(key);
+            }
+            else
+            {
+                throw new Exception("Слово отсутствует в словаре");
+            }
+        }
+
+        public string GetRussianWord(string key)
+        {
+            if(_dictionary.ContainsKey(key))
+            {
+                return _dictionary[key];
+            }
+            else
+            {
+                throw new Exception("Слово отсутствует в словаре");
+            }
+        }
+        public string GetEnglishWord(string key)
+        {
+            if(_dictionary.ContainsValue(key))
+            {
+                foreach (var item in _dictionary)
+                {
+                    if(item.Value == key)
+                    {
+                        return item.Key;
+                    }
+                }
+                return "";
+            }
+            else
+            {
+                throw new Exception("Слово отсутствует в словаре");
+            }
+        }
+        public void ShowWords()
+        {
+            foreach(var item in _dictionary)
+            {
+                Console.WriteLine($"{item.Key} - {item.Value}");
+            }
+        }
+    }
+}
---
+++ CoutryDictionary/Program.cs
@@ -0,0 +1,72 @@
+﻿using CountryDictionary;
+using System.Collections.Generic;
+
+internal class Program
+{
+    private static void Main()
+    {
+        Console.WriteLine("Добро пожаловать в мой словарь");
+        string rus, eng;
+        Glossary dictionary = new Glossary();
+        while (true)
+        {
+            Console.WriteLine("Нажмите 1 - чтобы вывести все слова словаря\n2 - чтобы дабавить слово в словарь\n" +
+                "3 - чтобы удалить слово\n4 - завершить работу со словарем\n5 - найти перевод слова");
+            int.TryParse(Console.ReadLine(), out int choice);
+            switch (choice)
+            {
+                case 1: 
+                    dictionary.ShowWords(); 
+                    break;
+                case 2:
+                    Console.WriteLine("Введите слово на русском: ");
+                    rus = Console.ReadLine();
+                    Console.WriteLine("Введите его перевод: ");
+                    eng = Console.ReadLine();
+                    dictionary.AddWords(rus, eng);
+                    break;
+                case 3:
+                    Console.WriteLine("Если слово будет на русском введите 1 любое другое значение значит что слово будет на английском");
+                    if (int.TryParse(Console.ReadLine(), out int x))
+                    {
+                        if (x == 1)
+                        {
+                            Console.WriteLine("Вводите:");
+                            rus = Console.ReadLine();
+                            dictionary.DellRussianWords(rus);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Enter:");
+                            eng = Console.ReadLine();
+                            dictionary.DellEnglishWords(eng);
+                        }
+                    }                    
+                    break;
+                case 4:
+                    return;
+                case 5:
+                    Console.WriteLine("Если слово будет на русском введите 1 любое другое значение значит что слово будет на английском");
+                    if(int.TryParse(Console.ReadLine(), out int y))
+                    {
+                        if(y == 1)
+                        {
+                            Console.WriteLine("Вводите:");
+                            rus = Console.ReadLine();
+                            Console.WriteLine(dictionary.GetRussianWord(rus));
+                        }
+                        else
+                        {
+                            Console.WriteLine("Enter:");
+                            eng = Console.ReadLine();
+                            Console.WriteLine(dictionary.GetEnglishWord(eng));
+                        }
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Неверно введеные данные");
+                    break;
+            }
+        }
+    }
+}
